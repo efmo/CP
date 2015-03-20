e2df4d39a02cacd92d72dfcba30c7aeb4f395f43
Franklin Morley <nozz@comcast.net>
2015-03-20 13:58:06 -0600

    Model descriptions and results

--- testscriptPCA.R
+++ testscriptPCA.R
@@ -9,7 +9,7 @@ modPPrf <- train(train5$classe[tSmall] ~.,method = "rf", data = trainPC)
 
 testPC5 <- predict(pp, train5[-tSmall,-48])
 
-confusionMatrix(train5$classe[-tSmall],predict(modPP,testPC))
+#confusionMatrix(train5$classe[-tSmall],predict(modPP,testPC5))
 
 pp2 <- preProcess(train6[tSmall,-49],method="pca")
 trainPC2 <- predict(pp2,train6[tSmall,-49])
@@ -20,7 +20,15 @@ modPPrf2 <- train(train6$classe[tSmall] ~.,method = "rf", data = trainPC2)
 testPC6 <- predict(pp2, train6[-tSmall,-49])
 
 
-confusionMatrix(train5$classe[-tSmall],predict(modPPgbm,testPC))
-confusionMatrix(train5$classe[-tSmall],predict(modPPrf,testPC))
-confusionMatrix(train6$classe[-tSmall],predict(modPPgbm2,testPC))
-confusionMatrix(train6$classe[-tSmall],predict(modPPrf2,testPC))
+confusionMatrix(train5$classe[-tSmall],predict(modPPgbm,testPC5))
+confusionMatrix(train5$classe[-tSmall],predict(modPPrf,testPC5))
+confusionMatrix(train6$classe[-tSmall],predict(modPPgbm2,testPC6))
+confusionMatrix(train6$classe[-tSmall],predict(modPPrf2,testPC6))
+
+pml_write_files = function(x){
+    n = length(x)
+    for(i in 1:n){
+        filename = paste0("predictions/problem_id_",i,".txt")
+        write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+    }
+}
